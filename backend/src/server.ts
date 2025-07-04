e1a4328c2ff4e62ab42346f949667981aac88f0f
alex-deev <ax.onestar@gmail.com>
2025-07-04 00:19:01 -0300

    style(backend): pequeña mejora en el estilo del código

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import TaskRouter from './routes/tasks.route';
 import UnknownRouter from './routes/unknow.route';
-import { InLogger, OutLogger } from './middlewares/logger.middleware';
+import { inLogger, outLogger } from './middlewares/logger.middleware';
 import { createTasksTable } from './database/statements';
 
 const server = express();
@@ -11,8 +11,8 @@ server.use(cors());
 server.use(express.json());
 
 // Middlewares para loggear cada petición entrante y saliente
-server.use(InLogger);
-server.use(OutLogger);
+server.use(inLogger);
+server.use(outLogger);
 
 // Endpoints para rutas de la funcionalidad CRUD de Tasks (tareas)
 server.use('/api/tasks', TaskRouter);
